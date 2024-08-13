297cb64025c7a5db7799867a0d1a78b070d8f6c9
Verc James <vercjames@gmail.com>
2024-08-12 20:42:07 -0500

    v0.1.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
-import React from "react"
+"use client"
+import React, { useEffect } from "react"
+import pjson from "@/package.json"
 
 // Application Component || Define Exports
 // =================================================================================================
 // =================================================================================================
 export default function Home() {
+  useEffect(() => {
+    console.log("==========================================================")
+    console.log(`Application-Client || Env: ${process.env.NEXT_PUBLIC_BUILD_ENV!}`)
+    console.log(`Application-Client || Version: v${pjson.version}`)
+    console.log("==========================================================")
+  }, [])
   return (
     <main>
+      {pjson.version}
+      <br />
       public.vercjames.com coming soon
     </main>
   )
